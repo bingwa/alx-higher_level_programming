a09809c99d9b02defaca2e1b12f710abc9cc3317
bingwa <munyaobryan@gmail.com>
2022-06-02 12:43:35 +0000

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #1/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    from calculator_1 import *
+    from calculator_1 import add, sub, mul, div
     count = len(argv)
 
     if count != 4:
